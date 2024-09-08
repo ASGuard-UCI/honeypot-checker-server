4ec34af16b834e9c75520779114d81b1711af588
Sam Der <sder@uci.edu>
2024-09-08 19:25:59 +0000

    Fix service activation

--- setup.sh
+++ setup.sh
@@ -8,12 +8,15 @@ pip install -r requirements.txt
 # Copy systemctl service to /etc/systemd/system/
 if [ "$USE_GCP_OR_AZURE" ]; then
 	sudo cp honeypot-checker-server-other.service /etc/systemd/system/
+	TASK="honeypot-checker-server-other"
 else
 	sudo cp honeypot-checker-server.service /etc/systemd/system/
+	TASK="honeypot-checker-server"
 fi
 
+
 # Enable systemctl task
-sudo systemctl enable --now honeypot-checker-server
-sudo systemctl restart honeypot-checker-server
+sudo systemctl enable --now $TASK
+sudo systemctl restart $TASK
 sleep 3
-sudo systemctl status honeypot-checker-server
+sudo systemctl status $TASK
