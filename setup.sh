aae130eb2b34c1dc2435eea9ea23cee2471bc960
Sam Der <sder@uci.edu>
2024-09-08 19:16:19 +0000

    Update script to apply to Azure

--- setup.sh
+++ setup.sh
@@ -6,7 +6,11 @@ python3 -m venv .venv
 pip install -r requirements.txt
 
 # Copy systemctl service to /etc/systemd/system/
-sudo cp honeypot-checker-server.service /etc/systemd/system/
+if [ "$USE_GCP_OR_AZURE" ]; then
+	sudo cp honeypot-checker-server-other.service /etc/systemd/system/
+else
+	sudo cp honeypot-checker-server.service /etc/systemd/system/
+fi
 
 # Enable systemctl task
 sudo systemctl enable --now honeypot-checker-server
