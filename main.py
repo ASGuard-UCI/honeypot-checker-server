1d7c9b0b740de2db24c5cf2ff529bafd98de2d1a
Sam Der <sder@uci.edu>
2024-09-13 08:28:00 -0400

    Only output warnings and above

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
+import logging
 import os
 
+import uvicorn
 from fastapi import FastAPI, status
 
 app = FastAPI()
 
-@app.get('/', status_code=status.HTTP_200_OK)
+
+@app.get("/", status_code=status.HTTP_200_OK)
 async def root():
     status = os.system("systemctl is-active --quiet scapy-writer")
-    return { "status": 1 if status == 0 else 0 }
+    return {"status": 1 if status == 0 else 0}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, log_level=logging.WARN, host="0.0.0.0", port=80)
